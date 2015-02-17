4c20c52d6ea002d9786d2e8e85c546786f451bde
Ari Morcos <arimorcos@gmail.com>
2015-02-17 09:36:30 -0500

    Added footer bar

--- javascript/main.js
+++ javascript/main.js
@@ -3,6 +3,9 @@ var main = function () {
     //Load in navigation bar
     $("div.navBar#main").load("html/navBar.html");
 
+    //Load in footer bar
+    $("div.footerBar#main").load("html/footerBar.html");
+
 }
 
 $(document).ready(main);
