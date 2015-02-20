24d4bc09f9879561624c0d67263bb935320cc9e3
Ari Morcos <arimorcos@gmail.com>
2015-02-19 23:45:13 -0500

    Animations now occur within borders
    Fixed animations such that they work within the borders.

--- blog/static/javascript/main.js
+++ blog/static/javascript/main.js
@@ -3,7 +3,7 @@ var main = function () {
     $('.showTooltip').tooltipster( {
         animation: 'fall',
         offsetX: 0,
-        offsetY: 2,
+        offsetY: 0,
         touchDevices: false,
         onlyOne: true,
         theme: 'tooltipster-default'
@@ -21,10 +21,13 @@ function changeAnimation() {
 
     // Process which class
     if (url.search('about') > 0) {
+        $('.navAbout > a').toggleClass("animateAboutMeColor");
         $('.navAbout > a > img').toggleClass("animateAboutMe");
     } else if (url.search('contact') > 0) {
+        $('.navBlog > a').toggleClass("animateBlogColor");
         $('.navBlog > a > img').toggleClass("animateBlog");
     } else if (url.search('research') > 0 ) {
+        $('.navResearch > a').toggleClass("animateResearchColor");
         $('.navResearch > a > img').toggleClass("animateResearch");
     }
 
