b3b29206b2417f8f1059d4b48f3340d0a174f3be
Ari Morcos <arimorcos@gmail.com>
2015-02-23 16:51:41 -0500

    Refactored to camelCase

--- blog.py
+++ blog.py
@@ -19,123 +19,123 @@ app.config.from_object(__name__)  # grab configuration file
 
 # redirect empty blog page to page 1
 @app.route('/blog/')
-def blognopage():
+def blogNoPage():
     return redirect('/blog/page1/')
 
 
 # redirect empty tag to page 1
 @app.route('/blog/tag/<tagname>/')
-def tagnopage(tagname):
+def tagNoPage(tagname):
     return redirect('/blog/tag/' + tagname + '/page1/')
 
 
 # create route for posts, which will grab all posts in the posts folder and render them in a template
-@app.route("/blog/page<pagenum>/")  # route to <webpage>/posts/
-def blog(pagenum):
+@app.route("/blog/page<pageNum>/")  # route to <webpage>/posts/
+def blog(pageNum):
 
-    postlist = get_post_list()
+    postList = getPostList()
 
-    post_view = render_postlist(postlist, pagenum)
+    postView = renderPostList(postList, pageNum)
 
-    return post_view
+    return postView
 
 
 @app.route('/blog/all/')
-def blog_all():
+def blogAll():
 
-    postlist = get_post_list()
-    post_view = render_postlist(postlist)
-    return post_view
+    postList = getPostList()
+    postView = renderPostList(postList)
+    return postView
 
 
 # create a route for tags, which will grab all posts which match a tag and render
-@app.route("/blog/tag/<tagname>/page<pagenum>/")
-def blog_tag(tagname, pagenum):
+@app.route("/blog/tag/<tagName>/page<pageNum>/")
+def blogTag(tagName, pageNum):
 
     # find posts which match tag
-    postlist = find_tag_match(tagname)
+    postList = findTagMatch(tagName)
 
-    post_view = render_postlist(postlist, pagenum, tagname)
+    postView = renderPostList(postList, pageNum, tagName)
 
-    return post_view
+    return postView
 
 
-def render_postlist(postlist, pagenum=False, tagname=False):
+def renderPostList(postList, pageNum=False, tagName=False):
 
     # Number of posts per page
     nPostsPerPage = 2
 
     # get recent posts
     nRecent = 10
-    allPosts = get_post_list()
-    recentposts = allPosts[:nRecent-1]
+    allPosts = getPostList()
+    recentPosts = allPosts[:nRecent-1]
 
-    if pagenum:
+    if pageNum:
 
         # slice to the correct post subset
-        startPost = (int(pagenum)-1) * nPostsPerPage
-        stopPost = max(len(postlist) - 1, int(pagenum) * nPostsPerPage)
-        postSubset = postlist[startPost:stopPost]
+        startPost = (int(pageNum) - 1) * nPostsPerPage
+        stopPost = max(len(postList) - 1, int(pageNum) * nPostsPerPage)
+        postSubset = postList[startPost:stopPost]
 
         # generate booleans for whether there should be a new or old posts button
-        if len(postlist) > nPostsPerPage and nPostsPerPage*int(pagenum) <= len(postlist):
+        if len(postList) > nPostsPerPage and nPostsPerPage*int(pageNum) <= len(postList):
             shouldOlder = True
-            if tagname:
-                olderpage = '/blog/tag/' + tagname + '/page' + str(int(pagenum)+1)
+            if tagName:
+                olderPage = '/blog/tag/' + tagName + '/page' + str(int(pageNum)+1)
             else:
-                olderpage = '/blog/page' + str(int(pagenum)+1)
+                olderPage = '/blog/page' + str(int(pageNum)+1)
         else:
             shouldOlder = False
-            olderpage = False
+            olderPage = False
 
-        if int(pagenum) > 1:
+        if int(pageNum) > 1:
             shouldNewer = True
-            if tagname:
-                newerpage = '/blog/tag/' + tagname + '/page' + str(int(pagenum)-1)
+            if tagName:
+                newerPage = '/blog/tag/' + tagName + '/page' + str(int(pageNum)-1)
             else:
-                newerpage = '/blog/page' + str(int(pagenum)-1)
+                newerPage = '/blog/page' + str(int(pageNum)-1)
         else:
             shouldNewer = False
-            newerpage = False
+            newerPage = False
     else:
         shouldNewer = False
         shouldOlder = False
-        newerpage = False
-        olderpage = False
-        postSubset = postlist
+        newerPage = False
+        olderPage = False
+        postSubset = postList
 
-    tagFreq = get_tag_frequency(True)
+    tagFreq = getTagFrequency(True)
     for tag in tagFreq:
         tagFreq[tag] = 'tag{0}'.format(tagFreq[tag])
 
     # return the posts rendered by the posts.html template
-    return render_template('posts.html', posts=postSubset, olderpage=olderpage,
-                           newerpage=newerpage, shouldNewer=shouldNewer, shouldOlder=shouldOlder,
-                           recentPosts=recentposts, tagfreq=tagFreq)
+    return render_template('posts.html', posts=postSubset, olderPage=olderPage,
+                           newerPage=newerPage, shouldNewer=shouldNewer, shouldOlder=shouldOlder,
+                           recentPosts=recentPosts, tagFreq=tagFreq)
 
 
-def get_post_list():
+def getPostList():
     # get list of all pages which come from the post directory within the flatpages root
-    postlist = [p for p in flatpages if p.path.startswith(POST_DIR)]
+    postList = [p for p in flatpages if p.path.startswith(POST_DIR)]
 
     # sort each post according to the date
-    postlist.sort(key=lambda item:item['date'], reverse=True)
+    postList.sort(key=lambda item:item['date'], reverse=True)
 
     # return
-    return postlist
+    return postList
 
 
-def find_tag_match(tagName):
+def findTagMatch(tagName):
     """
     Finds posts which match a given tag, case-insensitive
     :param tagName: string containing tag to match
-    :return: postlist
+    :return: postList
     """
 
-    postlist = get_post_list()
+    postList = getPostList()
 
     matchTag = []
-    for p in postlist:
+    for p in postList:
 
         if any(tag.lower() == tagName for tag in p['tags'].replace(' ', '').split(',')):
             matchTag.append(p)
@@ -143,16 +143,16 @@ def find_tag_match(tagName):
     return matchTag
 
 
-def get_tag_frequency(normalizeRange):
+def getTagFrequency(normalizeRange):
     """
     Finds the frequency of each tag
     normalizeRange: normalize the range of the tag frequencies
     :return: dict with key as tag and value being number of occurences
     """
-    postlist = get_post_list()
+    postList = getPostList()
 
     tagList = {}
-    for p in postlist:
+    for p in postList:
         for tag in p['tags'].replace(' ', '').split(','):
             if tag in tagList:
                 tagList[tag] += 1
