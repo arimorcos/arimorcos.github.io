a9f30e77c48ea133ff251f8a704f71536f026e7d
Ari Morcos <arimorcos@gmail.com>
2015-06-23 14:05:30 -0400

    Added twitter tags and updated fb tags
    Also, created description category in views.py

--- blog.py
+++ blog.py
@@ -5,5 +5,5 @@ from views import *
 
 if __name__ == "__main__":
     # run on the local host so that anyone can see it
-    # app.run(host='0.0.0.0', debug=False)
-    freezer.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
+    # freezer.run(debug=True)
