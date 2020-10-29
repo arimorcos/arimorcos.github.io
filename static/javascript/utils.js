d901b2a43317668a136e97435b3b36bfc1a9daf5
Ari Morcos <arimorcos@gmail.com>
2020-10-29 16:03:29 -0700

    Added ability to directly copy bibtex

--- static/javascript/utils.js
+++ static/javascript/utils.js
@@ -6,3 +6,27 @@ function showHideBibtex(curr_paper) {
         bibtex_element.style.display = ""
     }
 }
+
+function copyBibtex(curr_paper) {
+    let bibtex_element = document.querySelector(curr_paper.concat("> span > div.bibtex"))
+
+    // Make text area
+    const text_area = document.createElement('textarea');
+    text_area.value = bibtex_element.innerText;
+    text_area.setAttribute('readonly', '');
+    text_area.style.position = 'absolute';
+    text_area.style.left = '-9999px';
+    document.body.appendChild(text_area);
+
+    // Select
+    text_area.select();
+    text_area.setSelectionRange(0, 99999)
+
+    // Copy
+    document.execCommand("copy");
+
+    // Delete
+    document.body.removeChild(text_area);
+
+}
+
